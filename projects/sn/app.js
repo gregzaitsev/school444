2e3c999dccc856c7aa7f3f941a43532b8c1a9005
Greg Zaitsev <greg.zaitsev@gmail.com>
2019-11-06 07:33:59 +0300

    Instructions for social network

--- projects/sn/app.js
+++ projects/sn/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({ extended: true })); 
+
 const port = 8080;
 
 app.get('/', function (req, res) {
 	res.send('Hello World!');
 });
+
+// Для получения переменных из HTML формы
+// let login = req.body.login,
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
