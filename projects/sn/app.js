b675fdde12b4710f03c5152649fa56b1c2cd6811
Greg Zaitsev <greg.zaitsev@gmail.com>
2019-12-03 19:29:34 +0300

    Social network project - registration and user profile

--- projects/sn/app.js
+++ projects/sn/app.js
@@ -7,10 +7,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const port = 8080;
 
 app.get('/', function (req, res) {
-	res.send('Hello World!');
+	res.send(`
+		Hello World!<br>
+		${req.query.test}
+
+		`);
 });
 
 // Для получения переменных из HTML формы
 // let login = req.body.login,
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
